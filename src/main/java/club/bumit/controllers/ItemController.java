98c3d59072ede7f4102a3eb97d95b83c98258311
ichatz <git123hub456>
2014-09-07 10:47:18 +0300

    first entry arcGIS

--- src/main/java/club/bumit/controllers/ItemController.java
+++ src/main/java/club/bumit/controllers/ItemController.java
@@ -1,5 +1,6 @@
 package club.bumit.controllers;
 
+import club.bumit.util.BumitItem;
 import club.bumit.util.TwitterSingleton;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import twitter4j.*;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Display items provided.
@@ -21,27 +21,42 @@ public class ItemController {
     public String home(final Model model) {
         // The factory instance is re-useable and thread safe.
         final Twitter twitter = TwitterSingleton.getInstance().getTwitterFactory().getInstance();
-        final Query query = new Query("#disruptsf2014");
+        final Query query = new Query("#bumit");
         try {
             final QueryResult result = twitter.search(query);
-            final List<Status> tweets = result.getTweets();
-            model.addAttribute("tweets", tweets);
-            final ArrayList<club.bumit.util.Location> locations = new ArrayList<club.bumit.util.Location>();
-            for (final Status status : tweets) {
-                final club.bumit.util.Location point = new club.bumit.util.Location();
-                final GeoLocation pos = status.getGeoLocation();
-                if (pos != null) {
-                    point.setLatitude(status.getGeoLocation().getLatitude());
-                    point.setLongitude(status.getGeoLocation().getLongitude());
-                    locations.add(point);
-                }
+            final ArrayList<BumitItem> bumitItems = new ArrayList<BumitItem>();
+
+            for (final Status status : result.getTweets()) {
+                final int imagePos = status.getText().indexOf("co");
+                if (imagePos > 0) {
+                    final BumitItem item = new BumitItem();
+                    item.setText(status.getText().substring(0, imagePos));
+                    item.setName(status.getUser().getName());
+                    item.setHandle(status.getUser().getScreenName());
+                    item.setImageUrl(status.getText().substring(imagePos));
+
+                    final GeoLocation pos = status.getGeoLocation();
+                    if (pos != null) {
+                        item.setLatitude(status.getGeoLocation().getLatitude());
+                        item.setLongitude(status.getGeoLocation().getLongitude());
+
+                    } else if (status.getPlace() != null) {
+                        final GeoLocation[][] posBox = status.getPlace().getGeometryCoordinates();
+                        if (posBox != null) {
+                            item.setLatitude(posBox[0][0].getLatitude());
+                            item.setLongitude(posBox[0][0].getLongitude());
+                        }
+                    }
+
+                    bumitItems.add(item);
+               }
             }
-            model.addAttribute("locations", locations);
+
+            model.addAttribute("items", bumitItems);
 
         } catch (Exception ex) {
-            System.err.print(ex);
-            model.addAttribute("tweets", new ArrayList<Status>());
-            model.addAttribute("locations", new ArrayList<club.bumit.util.Location>());
+            ex.printStackTrace();
+            model.addAttribute("items", new ArrayList<BumitItem>());
         }
 
         return "items";
---
+++ src/main/java/club/bumit/util/BumitItem.java
@@ -0,0 +1,70 @@
+package club.bumit.util;
+
+import java.io.Serializable;
+
+/**
+ * Storing location of tweets.
+ */
+public class BumitItem
+        implements Serializable {
+
+    private String imageUrl;
+
+    private String text;
+
+    private String name;
+
+    private String handle;
+
+    private double latitude;
+
+    private double longitude;
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHandle() {
+        return handle;
+    }
+
+    public void setHandle(String handle) {
+        this.handle = handle;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
