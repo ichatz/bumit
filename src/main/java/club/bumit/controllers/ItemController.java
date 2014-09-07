36769485726de5d7afde127e7604af4107f61a5b
ichatz <git123hub456>
2014-09-07 11:35:42 +0300

    implementing georss to pass data to arcgis

--- src/main/java/club/bumit/controllers/ItemController.java
+++ src/main/java/club/bumit/controllers/ItemController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import twitter4j.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Display items provided.
@@ -17,20 +18,20 @@ import java.util.ArrayList;
 @Controller
 public class ItemController {
 
-    @RequestMapping("/items")
-    public String home(final Model model) {
+    private List<BumitItem> getItems() {
         // The factory instance is re-useable and thread safe.
         final Twitter twitter = TwitterSingleton.getInstance().getTwitterFactory().getInstance();
         final Query query = new Query("#bumit");
+        final ArrayList<BumitItem> bumitItems = new ArrayList<BumitItem>();
+
         try {
             final QueryResult result = twitter.search(query);
-            final ArrayList<BumitItem> bumitItems = new ArrayList<BumitItem>();
-
             for (final Status status : result.getTweets()) {
-                final int imagePos = status.getText().indexOf("co");
+                final int imagePos = status.getText().indexOf("http://t.co");
+                System.out.println(imagePos + ": " + status.getText());
                 if (imagePos > 0) {
                     final BumitItem item = new BumitItem();
-                    item.setText(status.getText().substring(0, imagePos));
+                    item.setText(status.getText().substring(0, imagePos - 1));
                     item.setName(status.getUser().getName());
                     item.setHandle(status.getUser().getScreenName());
                     item.setImageUrl(status.getText().substring(imagePos));
@@ -45,20 +46,40 @@ public class ItemController {
                         if (posBox != null) {
                             item.setLatitude(posBox[0][0].getLatitude());
                             item.setLongitude(posBox[0][0].getLongitude());
+                        } else {
+                            System.out.println(status.getPlace().getName());
                         }
+                    } else if (status.getUser().getLocation() != null) {
+                        System.out.println(status.getUser().getLocation());
                     }
 
-                    bumitItems.add(item);
-               }
-            }
+                    if (item.getLongitude() == 0) {
+                        item.setLatitude(37.774929);
+                        item.setLongitude(-122.419416);
+                    }
 
-            model.addAttribute("items", bumitItems);
+                    System.out.println(item.getLatitude() + " " + item.getLongitude());
+                    System.out.println("---");
+                    bumitItems.add(item);
+                }
+            }
 
         } catch (Exception ex) {
             ex.printStackTrace();
-            model.addAttribute("items", new ArrayList<BumitItem>());
         }
 
+        return bumitItems;
+    }
+
+    @RequestMapping("/items.rss")
+    public String rss(final Model model) {
+        model.addAttribute("items", getItems());
+        return "itemsrss";
+    }
+
+    @RequestMapping("/items")
+    public String home(final Model model) {
+        model.addAttribute("items", getItems());
         return "items";
     }
 
