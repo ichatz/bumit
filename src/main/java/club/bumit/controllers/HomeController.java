b2f5f2110328cb9018e9b4fb4398abf813b3eede
ichatz <git123hub456>
2014-09-07 03:48:35 +0300

    fixing twitter connect

--- src/main/java/club/bumit/controllers/HomeController.java
+++ src/main/java/club/bumit/controllers/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.social.twitter.api.TwitterProfile;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.inject.Inject;
 
@@ -15,6 +16,7 @@ import javax.inject.Inject;
  * @author ichatz@gmail.com
  */
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
     private Twitter twitter;
@@ -27,7 +29,7 @@ public class HomeController {
         this.connectionRepository = connectionRepository;
     }
 
-    @RequestMapping("/")
+    @RequestMapping(method=RequestMethod.GET)
     public String home(final Model model) {
         if (connectionRepository.findPrimaryConnection(Twitter.class) == null) {
             return "redirect:/connect/twitter";
